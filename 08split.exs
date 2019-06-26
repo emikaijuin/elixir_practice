762dea7f58a03fc77a47632e6ace73238a0d9c29
Emika Ijuin <emikaijuin@gmail.com>
2019-06-26 11:19:42 -0400

    language processor exercise

---
+++ 08split.exs
@@ -0,0 +1,39 @@
+defmodule LanguageProcessor do
+
+  @stop_words ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+  def run(string) do
+    string
+    |> String.split
+    |> Enum.map(fn word -> String.downcase(word) end)
+    |> remove_stop_words
+    |> count_frequency
+  end
+
+  def remove_stop_words(lst, acc \\ [])
+  def remove_stop_words([], acc), do: acc
+  def remove_stop_words([word | remaining], acc) do
+    acc = cond do
+            word in @stop_words -> acc
+            true -> acc ++ [word]
+          end
+
+    remove_stop_words(remaining, acc)
+  end
+
+  def count_frequency(lst, freq \\ %{})
+  def count_frequency([], freq), do: freq
+  def count_frequency([word | remaining], freq) do
+    freq = case Map.fetch(freq, word) do
+            {:ok, x} ->
+              Map.update(freq, word, x + 1)
+            :error ->
+              Map.put(freq, word, 1)
+          end
+
+    count_frequency(remaining, freq)
+  end
+
+end
+
+IO.inspect LanguageProcessor.run("Whether you believe it or not it isn't the biggest issue")
